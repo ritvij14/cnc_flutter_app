70add7263472f01849e92c1ca9a384429d09cb47
Liam <lpcbusiness777@gmail.com>
2021-02-22 22:17:33 -0600

    added share feature along with starting the weekly goals section

--- lib/screens/home/goal_calendar_screen.dart
+++ lib/screens/home/goal_calendar_screen.dart
@@ -4,14 +4,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:clipboard/clipboard.dart';
+import 'package:share/share.dart';
 
 // void main() => runApp(GoalCalendar());
+
+void main() => runApp(GoalCalendar());
 
 class GoalCalendar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CalendarPage();
-
+    //return CalendarPage();
     return MaterialApp(
       title: 'Goal Calendar',
       theme: ThemeData(
@@ -35,6 +37,12 @@ class _CalendarPageState extends State<CalendarPage> {
   TextEditingController _eventController;
   SharedPreferences prefs;
 
+  CalendarController _controller2;
+  Map<DateTime, List<dynamic>> _events2;
+  List<dynamic> _selectedEvents2;
+  TextEditingController _eventController2;
+  SharedPreferences prefs2;
+
   @override
   void initState() {
     super.initState();
@@ -42,7 +50,21 @@ class _CalendarPageState extends State<CalendarPage> {
     _eventController = TextEditingController();
     _events = {};
     _selectedEvents = [];
+
+    _controller2 = CalendarController();
+    _eventController2 = TextEditingController();
+    _events2 = {};
+    _selectedEvents2 = [];
     initPrefs();
+
+    _events2[_controller2.selectedDay] = [
+    ];
+    _events2[_controller2.selectedDay]
+        .add("Goal 1");
+    _events2[_controller2.selectedDay]
+        .add("Goal 2");
+    _events2[_controller2.selectedDay]
+        .add("Goal 3");
   }
 
   initPrefs() async {
@@ -133,6 +155,21 @@ class _CalendarPageState extends State<CalendarPage> {
               color: Theme.of(context).accentColor,
               alignment: Alignment.bottomLeft,
               child: Text(
+                'Weekly Goals',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            _buildWeeklyView(),
+
+            Container(
+              padding: EdgeInsets.all(15.0),
+              color: Theme.of(context).accentColor,
+              alignment: Alignment.bottomLeft,
+              child: Text(
                 'Personal Goals',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
@@ -144,26 +181,6 @@ class _CalendarPageState extends State<CalendarPage> {
             _events[_controller.selectedDay] != null
                 ? _buildListView()
                 : SizedBox(height: 0),
-            // ..._selectedEvents.map((event) => Padding(
-            //       padding:
-            //           const EdgeInsets.symmetric(vertical: 5, horizontal: 70),
-            //       child: Container(
-            //         width: MediaQuery.of(context).size.width,
-            //         decoration: BoxDecoration(
-            //             borderRadius: BorderRadius.circular(10),
-            //             color: Colors.white,
-            //             border: Border.all(color: Colors.blue[800])),
-            //         child: Padding(
-            //             padding: const EdgeInsets.all(8.0),
-            //             child: Text(
-            //               event,
-            //               style: TextStyle(
-            //                   color: Colors.pinkAccent,
-            //                   fontWeight: FontWeight.bold,
-            //                   fontSize: 16),
-            //             )),
-            //       ),
-            //     )),
           ],
         ),
       ),
@@ -183,7 +200,6 @@ class _CalendarPageState extends State<CalendarPage> {
         shrinkWrap: true,
         itemCount: _events[_controller.selectedDay].length,
         itemBuilder: (context, index) {
-          final sweets = _events[index];
 
           return Slidable(
             actionPane: SlidableDrawerActionPane(),
@@ -193,7 +209,7 @@ class _CalendarPageState extends State<CalendarPage> {
                 title: Text(_events[_controller.selectedDay][index].toString()),
               ),
             ),
-            secondaryActions: <Widget>[
+            actions: <Widget>[
               IconSlideAction(
                   caption: 'Copy',
                   color: Colors.grey[400],
@@ -201,7 +217,17 @@ class _CalendarPageState extends State<CalendarPage> {
                   onTap: () {
                     FlutterClipboard.copy(
                         _events[_controller.selectedDay][index].toString());
+                    _showSnackBar(context, 'Copied to Clipboard');
                   }),
+            ],
+            secondaryActions: <Widget>[
+              IconSlideAction( caption: 'Share',
+              color: Colors.indigo,
+              icon: Icons.share,
+              onTap: () {
+                Share.share(_events[_controller.selectedDay][index].toString(),
+                    subject: 'Check out my new goal on the Enact diet tracking app!');
+              }),
               IconSlideAction(
                   caption: 'Delete',
                   color: Colors.red,
@@ -214,6 +240,61 @@ class _CalendarPageState extends State<CalendarPage> {
                       if (_events[_controller.selectedDay] != null) {
                         _selectedEvents = _events[_controller.selectedDay];
                       }
+                      _showSnackBar(context, 'Deleted Goal');
+                    });
+                  }),
+            ],
+          );
+        });
+  }
+
+  Widget _buildWeeklyView() {
+
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: _events2[_controller2.selectedDay].length,
+        itemBuilder: (context, index) {
+
+          return Slidable(
+            actionPane: SlidableDrawerActionPane(),
+            child: Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text(_events2[_controller2.selectedDay][index].toString()),
+              ),
+            ),
+            actions: <Widget>[
+              IconSlideAction(
+                  caption: 'Copy',
+                  color: Colors.grey[400],
+                  icon: Icons.content_copy,
+                  onTap: () {
+                    FlutterClipboard.copy(
+                        _events2[_controller2.selectedDay][index].toString());
+                    _showSnackBar(context, 'Copied to Clipboard');
+                  }),
+            ],
+            secondaryActions: <Widget>[
+              IconSlideAction( caption: 'Share',
+                  color: Colors.indigo,
+                  icon: Icons.share,
+                  onTap: () {
+                    Share.share(_events2[_controller2.selectedDay][index].toString(),
+                        subject: 'Check out my new goal on the Enact diet tracking app!');
+                  }),
+              IconSlideAction(
+                  caption: 'Delete',
+                  color: Colors.red,
+                  icon: Icons.delete,
+                  onTap: () {
+                    _events2[_controller2.selectedDay].removeAt(index);
+                    prefs.setString("events", json.encode(encodeMap(_events2)));
+                    _eventController2.clear();
+                    setState(() {
+                      if (_events2[_controller2.selectedDay] != null) {
+                        _selectedEvents2 = _events2[_controller2.selectedDay];
+                      }
+                      _showSnackBar(context, 'Deleted Goal');
                     });
                   }),
             ],
